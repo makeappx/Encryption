b52813e073ad8d47af3bcb27598da56c03ec34de
Anton <50206467+makeappx@users.noreply.github.com>
2022-01-30 14:36:45 +0300

    Add files via upload

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 69);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(486, 188);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Шифр цезаря",
+            "Шифр Виженера",
+            "Rot13(Русские буквы)",
+            "Гаммирование",
+            "Квадрат Полибия",
+            "Тарабарская грамота"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(308, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.richTextBox2.Location = new System.Drawing.Point(12, 276);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(486, 188);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(326, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(10, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Шифр";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(323, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ключ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Исходный текст";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 260);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Дешифрованный текст";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 469);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Алгоритмы шифрования";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                richTextBox1.Text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                textBox1.Visible = true;
+                textBox1.Text = "10";
+                label2.Visible = true;
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                richTextBox1.Text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                label2.Visible = true;
+                textBox1.Visible = true;
+                textBox1.Text = "ABC";
+            }
+            if (comboBox1.SelectedIndex == 2)
+            {
+                richTextBox1.Text = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
+                label2.Visible = false;
+                textBox1.Visible = false;
+            }
+            if(comboBox1.SelectedIndex == 3)
+            {
+                textBox1.Text = "6,12,22,5,3";
+                richTextBox1.Text = "ДЬУЙЬДЭУЙРЧЯТЩЬ";
+                label2.Visible = true;
+                textBox1.Visible = true;
+            }
+            if (comboBox1.SelectedIndex == 4)
+            {
+                richTextBox1.Text = "13 34 22 24 44 34 15 42 22 34 43 45 32";
+                textBox1.Visible = false; 
+                label2.Visible = false;
+            }
+            if (comboBox1.SelectedIndex == 5)
+            {
+                richTextBox1.Text = "МЫЩАЛ ЧОСОШ ЫЧПИЕК";
+                textBox1.Visible = false; 
+                label2.Visible = false;
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedIndex == 0)
+                    richTextBox2.Text = Cesarus(richTextBox1.Text, int.Parse(textBox1.Text));
+                if (comboBox1.SelectedIndex == 1)
+                    richTextBox2.Text = Vigenere(richTextBox1.Text, textBox1.Text).ToUpper();
+                if (comboBox1.SelectedIndex == 2)
+                    richTextBox2.Text = Rot13Rus(richTextBox1.Text);
+                if (comboBox1.SelectedIndex == 3)
+                    richTextBox2.Text = Gamma(richTextBox1.Text, textBox1.Text.Split(',').Select(x => int.Parse(x)).ToArray());
+                if (comboBox1.SelectedIndex == 4)
+                    richTextBox2.Text = Polybius(richTextBox1.Text.Split(' ').Select(x => int.Parse(x)).ToArray());
+                if (comboBox1.SelectedIndex == 5)
+                    richTextBox2.Text = Tarabar(richTextBox1.Text);
+
+                richTextBox1.SelectionStart = richTextBox1.TextLength;
+            }
+            catch
+            {
+            }
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedIndex == 0)
+                    richTextBox1.Text = Cesarus(richTextBox2.Text, 26 - int.Parse(textBox1.Text));
+                if (comboBox1.SelectedIndex == 1)
+                    richTextBox1.Text = Vigenere(richTextBox2.Text, textBox1.Text, false).ToUpper();
+                if (comboBox1.SelectedIndex == 2)
+                    richTextBox1.Text = Rot13Rus(richTextBox2.Text, false);
+                if (comboBox1.SelectedIndex == 3)
+                    richTextBox1.Text = Gamma(richTextBox2.Text, textBox1.Text.Split(',').Select(x => int.Parse(x)).ToArray());
+                if (comboBox1.SelectedIndex == 4)
+                    richTextBox1.Text = DePolybius(richTextBox2.Text);
+                if (comboBox1.SelectedIndex == 5)
+                    richTextBox1.Text = Tarabar(richTextBox2.Text);
+
+                richTextBox2.SelectionStart = richTextBox2.TextLength;
+            }
+            catch
+            {
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1_TextChanged(null, null);
+        }
+
+        string Cesarus(string s, int offset)
+        {
+            if (offset < 1 || offset > 26) return s;
+            return new string(s.ToList().Select(x => char.IsLetter(x) ? (char)(((x + offset - (char.IsLower(x) ? 'a' : 'A')) % 26) + (char.IsLower(x) ? 'a' : 'A')) : x).ToArray());
+        }
+        string Rot13Rus(string s, bool encrypt = true)
+        {
+            return new string(s.ToList().Select(x =>
+            {
+                bool isUpper = char.IsUpper(x);
+                x = char.ToLower(x);
+                x = char.IsLetter(x) ? (char)(((x + (encrypt ? 13 : -13) - 'А') % 32) + 'А') : x;
+                return isUpper ? char.ToUpper(x) : char.ToLower(x);
+            }).ToArray());
+        }
+        private string Vigenere(string text, string password, bool encrypt = true)
+        {
+            string GetRepeatKey(string s, int n)
+            {
+                var p = s;
+                while (p.Length < n)
+                    p += p;
+
+                return p.Substring(0, n);
+            }
+            var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var gamma = GetRepeatKey(password, text.Length);
+            var retValue = "";
+            var q = letters.Length;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var letterIndex = letters.IndexOf(text[i]);
+                var codeIndex = letters.IndexOf(gamma[i]);
+                if (letterIndex < 0)
+                    retValue += text[i].ToString();
+                else
+                    retValue += letters[(q + letterIndex + ((encrypt ? 1 : -1) * codeIndex)) % q].ToString();
+            }
+
+            return retValue;
+        }
+        private string Gamma(string s, int[] gamma)
+        {
+            string kek = "";
+            for (int i = 0; i < s.Length; i++)
+                kek += Encoding.GetEncoding(1251).GetString(new byte[] { 
+                    (byte)(Encoding.GetEncoding(1251).GetBytes(new char[] { s[i] })[0] ^ gamma[i % gamma.Length]) });
+            return kek;
+        }
+        private string Tarabar(string s)
+        {
+            string kek = "бвгджзклмнщшчцхфтсрп";
+            s = s.ToLower();
+            string s1 = "";
+            for (int i = 0; i < s.Length; i++)
+                if (kek.Contains(s[i]))
+                    s1 += kek[(10 + kek.IndexOf(s[i])) % 20];
+                else
+                    s1 += s[i];
+            return s1;
+        }
+        private char[][] dict = new char[][] { new char[] { 'a', 'b', 'c', 'd', 'e' },
+                                               new char[] { 'f', 'g', 'h', 'i', 'k' },
+                                               new char[] { 'l', 'm', 'n', 'o', 'p' },
+                                               new char[] { 'q', 'r', 's', 't', 'u' },
+                                               new char[] { 'v', 'w', 'x', 'y', 'z' }};
+        private string Polybius(int[] keys) =>
+            new string(keys.Select(x => dict[(x / 10) - 1][--x % 10]).ToArray());
+        private string DePolybius(string s)
+        {
+            string k = "";
+            foreach(char c in s)
+            {
+                for (int i = 0; i < dict.Length; i++)
+                    if (dict[i].Contains(c))
+                        for (int j = 0; j < dict[i].Length; j++)
+                            if(dict[i][j] == c)
+                                k += (i + 1).ToString() + (j + 1).ToString() + " ";
+            }
+            return k;
+        }
+    }
+}
